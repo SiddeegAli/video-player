b70389042164310b8c9e797acf57d941d8d6b900
mohammed-xp <m7md20039579@gmail.com>
2025-09-24 18:32:01 +0200

    createing a simple decode video frame function as a starting point

--- video-player/src/main.cpp
+++ video-player/src/main.cpp
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
 extern "C" {
-#include<libavcodec/avcodec.h>
 #include<libavformat/avformat.h>
 }
 
+int decode_video_frame(const char* file_name, AVFrame** out_video_frame);
+
 int main() {
 	fprintf(stdout , "FFmpeg C++ Video Player");
+	
+	AVFrame* video_frame;
+
+	int ret = decode_video_frame("toubun no Hanayome Movie.mp4", &video_frame);
+	
+	if (ret < 0) {
+		fprintf(stderr, "Failed to decode video frame\n");
+		return ret;
+	}
+
+	fprintf(stdout, "Video frame decoded successfully\n");
+
+	//print the videos resolution
+	fprintf(stdout, "Video Resolution: %dx%d\n", video_frame->width, video_frame->height);
+
+	// Free the allocated frame
+	av_frame_free(&video_frame);
+
 	return 0;
 }
